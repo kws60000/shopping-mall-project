51c8922771b47f28853bc9f761c57c52dbe224b7
dylan <kws60000@naver.com>
2020-08-22 23:12:34 +0900

    헤더, 사이드 컴포넌트와 각 페이지의 링크 버튼을 연결합니다.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
   return (
     <div className="Header-wrapper">
       <div className="Header-category">
-        <li>홈</li>
-        <li>포인트 충전</li>
-        <li>구매 내역</li>
-        <li>상품 등록</li>
-        <li>장바구니</li>
-        <li>고객센터</li>
-        <li>이벤트</li>
-        <li>리뷰</li>
+        <Link to="/" className="Header-category-link">
+          <div className="Header-category-list">홈</div>
+        </Link>
+        <Link to="/Charge" className="Header-category-link">
+          <div className="Header-category-list">포인트 충전</div>
+        </Link>
+        <Link to="/List" className="Header-category-link">
+          <div className="Header-category-list">구매 내역</div>
+        </Link>
+        <Link to="/Add" className="Header-category-link">
+          <div className="Header-category-list">상품 등록</div>
+        </Link>
+
+        <div className="Header-category-list">장바구니</div>
+        <div className="Header-category-list">고객센터</div>
+        <div className="Header-category-list">이벤트</div>
+        <div className="Header-category-list">리뷰</div>
       </div>
     </div>
   );
