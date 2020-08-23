6cffe594f98858e7a3ff2825f27230dc61450478
dylan <kws60000@naver.com>
2020-08-23 16:32:36 +0900

    상품 등록 페이지의 UI를 구성합니다. 첨부 이미지 파일 미리보기, 입력 버튼을 추가합니다.

--- src/Routes/AddPage/AddPage.js
+++ src/Routes/AddPage/AddPage.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import "./AddPage.css";
 
 const AddPage = () => {
-  return <div>AddPage</div>;
+  const [img64, setImg64] = useState(null);
+  const onChangeImg = (e) => {
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const base64 = reader.result;
+      if (base64) {
+        setImg64(base64.toString());
+      }
+    };
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
+  };
+  return (
+    <div className="AddPage-wrapper">
+      <div className="AddPage-title">Product Enroll</div>
+      <div className="AddPage-container">
+        <div>
+          <input
+            className="AddPage-container-file"
+            type="file"
+            onChange={onChangeImg}
+          ></input>
+        </div>
+        <img alt="" src={img64} className="AddPage-container-img"></img>
+
+        <div>
+          <input
+            className="AddPage-container-text"
+            placeholder="상품 이름"
+          ></input>
+        </div>
+        <div>
+          <input
+            className="AddPage-container-text"
+            placeholder="판매 가격"
+          ></input>
+        </div>
+
+        <button className="AddPage-container-button">Enroll</button>
+      </div>
+    </div>
+  );
 };
 
 export default AddPage;
